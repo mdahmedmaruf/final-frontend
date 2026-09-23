769a1e9b8dbca2644f044b6547b9144b7094ba27
Maruf Ahmed <mdahmedmaruf@gmail.com>
2026-09-23 23:57:37 +0600

    update base url

--- src/services/api.js
+++ src/services/api.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000'
-export const BASE_URL = 'http://127.0.0.1:8000'
+export const BASE_URL = 'https://final-assignment-a6bg.onrender.com'
